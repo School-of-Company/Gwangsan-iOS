aebfb4918fe0bb013073131460a4c90c8a01db1a
hotcat0421 <s24044@gsm.hs.kr>
2025-04-12 15:04:14 +0900

    💄 :: [#5] 디자인 리소스 추가

---
+++ Projects/App/Resources/Color/Untitled.swift
@@ -0,0 +1,8 @@
+//
+//  Untitled.swift
+//  Gwangsan
+//
+//  Created by 박정우 on 4/10/25.
+//  Copyright © 2025 schoolcompany. All rights reserved.
+//
+
---
+++ Projects/App/Sources/Application/GwangsanApp.swift
@@ -0,0 +1,18 @@
+//
+//  GwangsanApp.swift
+//  Gwangsan
+//
+//  Created by 박정우 on 4/10/25.
+//  Copyright © 2025 schoolcompany. All rights reserved.
+//
+
+import SwiftUI
+
+@main
+struct GwangsanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            SwiftUIView()
+        }
+    }
+}
