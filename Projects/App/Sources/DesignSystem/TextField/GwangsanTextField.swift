dc570b9c7973551258f270c18bdc05ea3130d004
hotcat0421 <s24044@gsm.hs.kr>
2025-04-15 23:08:42 +0900

    💄 :: [#10] Button 추가

--- Projects/App/Sources/DesignSystem/TextField/GwangsanTextField.swift
+++ Projects/App/Sources/DesignSystem/TextField/GwangsanTextField.swift
@@ -13,39 +13,42 @@ struct GwangsanTextField: View {
     @FocusState var isFocused: Bool
     var title: String
     var placeholder: String
+    var horizontalPadding: CGFloat
 
     private var borderColor: Color {
-           isFocused ? GwangsanAsset.Color.mainYellow400.swiftUIColor : .black
+        isFocused ? GwangsanAsset.Color.mainYellow400.swiftUIColor : .black
     }
 
     public init(
         _ placeholder: String = "",
         text: Binding<String>,
-        title: String = ""
+        title: String = "",
+        horizontalPadding: CGFloat = 20
     ) {
         self._text = text
         self.placeholder = placeholder
         self.title = title
+        self.horizontalPadding = horizontalPadding
     }
-    
+
     var body: some View {
         VStack(alignment: .leading, spacing: 4) {
             Text(title)
                 .font(.system(size: 14))
                 .fontWeight(.medium)
 
-            TextField("", text: $text, prompt: Text(placeholder))
-                .font(.system(size: 14))
-                .fontWeight(.medium)
-                .frame(height: 56)
-                .padding(.horizontal, 22)
-                .focused($isFocused)
-                .overlay {
-                    RoundedRectangle(cornerRadius: 12)
-                        .strokeBorder(borderColor)
-                }
-                .cornerRadius(12)
+            ZStack {
+                RoundedRectangle(cornerRadius: 12)
+                    .strokeBorder(borderColor)
+
+                TextField("", text: $text, prompt: Text(placeholder))
+                    .font(.system(size: 14))
+                    .fontWeight(.medium)
+                    .padding(.horizontal, 22)
+                    .focused($isFocused)
+            }
+            .frame(height: 56)
         }
-        .padding(.horizontal, 20)
+        .padding(.horizontal, horizontalPadding)
     }
 }
--- Projects/App/Sources/SwiftUIView.swift
+++ Projects/App/Sources/SwiftUIView.swift
@@ -15,13 +15,42 @@ struct SwiftUIView: View {
             .gwangsanFont(style: .titleLarge)
             .gwangsanColor(GwangsanAsset.Color.mainYellow400)
         GwangsanAsset.Assets.chat.swiftUIImage
-     
+        
         
         GwangsanTextField(
             "이름을 입력해주세요",
             text: $inputText,
-            title: "이름"
+            title: "이름",
+            horizontalPadding: 20
         )
+        
+        GwangsanButton(
+            text: "다음",
+            buttonState: true,
+            horizontalPadding: 20,
+            height: 54
+        )
+        
+        HStack(spacing: 12) {
+            GwangsanTextField(
+                "전화번호를 입력해주세요",
+                text: $inputText,
+                title: "전화번호",
+                horizontalPadding: 0
+            )
+            .frame(height: 56)
+            .frame(maxWidth: .infinity)
+            
+            GwangsanButton(
+                text: "인증",
+                buttonState: false,
+                horizontalPadding: 0,
+                height: 56
+            )
+            .padding(.top, 16)
+            .frame(width: 80)
+        }
+        .padding(.horizontal, 20)
     }
 }
 
