45ceb3a4eb3d45fc3fe7298dc1d8451b53a11e20
hotcat0421 <s24044@gsm.hs.kr>
2025-04-16 21:00:39 +0900

    Button 수정

---
+++ Projects/App/Sources/DesignSystem/ViewModifier.swift
@@ -0,0 +1,19 @@
+//
+//  ViewModifier.swift
+//  Gwangsan
+//
+//  Created by 박정우 on 4/16/25.
+//  Copyright © 2025 schoolcompany. All rights reserved.
+//
+
+import SwiftUI
+
+struct ViewModifier: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ViewModifier()
+}
