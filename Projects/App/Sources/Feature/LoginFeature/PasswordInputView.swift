a4638312b0ec42b26a595bb7b07a1cc5778f05b9
hotcat0421 <s24044@gsm.hs.kr>
2025-04-16 23:15:08 +0900

    💄 :: [#15] 비밀번호퍼블리싱

--- Projects/App/Sources/Feature/LoginFeature/PasswordInputView.swift
+++ Projects/App/Sources/Feature/LoginFeature/PasswordInputView.swift
@@ -9,8 +9,45 @@
 import SwiftUI
 
 struct PasswordInputView: View {
+    @State var password: String = ""
+    
     var body: some View {
-        Text("패스워드")
+        NavigationStack{
+            VStack {
+                VStack(spacing: 54) {
+                    VStack(alignment: .leading) {
+                        Text("로그인")
+                            .gwangsanFont(style: .titleMedium)
+                        
+                        Text("비밀번호를 입력해주세요")
+                            .gwangsanFont(style: .label)
+                            .gwangsanColor(GwangsanAsset.Color.gray500)
+                    }
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    .padding(.horizontal, 24)
+                    
+                    GwangsanTextField(
+                        "비밀번호를 입력해주세요",
+                        text: $password,
+                        title: "비밀번호",
+                        horizontalPadding: 24
+                    )
+                }
+                .padding(.top, 16)
+                
+                Spacer()
+                
+                GwangsanButton(
+                    text: "다음",
+                    buttonState: !password.isEmpty,
+                    horizontalPadding: 24,
+                    height: 52,
+                    destination: StartView()     // MainFeature로 변경해야함
+                )
+                .padding(.bottom, 30)
+            }
+            .modifier(BackButtonModifier())
+        }
     }
 }
 
