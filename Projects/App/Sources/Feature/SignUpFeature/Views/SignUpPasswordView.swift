ec927f79c0546672f0f004fe169cdfc782a9ae99
hotcat0421 <s24044@gsm.hs.kr>
2025-04-17 20:37:37 +0900

    💄 :: [#17] 회원가입 비밀번호 입력 구현

---
+++ Projects/App/Sources/Feature/SignUpFeature/Views/SignUpPasswordView.swift
@@ -0,0 +1,19 @@
+//
+//  SignUpPasswordView.swift
+//  Gwangsan
+//
+//  Created by 박정우 on 4/17/25.
+//  Copyright © 2025 schoolcompany. All rights reserved.
+//
+
+import SwiftUI
+
+struct SignUpPasswordView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SignUpPasswordView()
+}
---
+++ Projects/App/Sources/Feature/SignUpFeature/Views/SignUpViewModel.swift
@@ -0,0 +1,9 @@
+//
+//  SignUpViewModel.swift
+//  Gwangsan
+//
+//  Created by 박정우 on 4/17/25.
+//  Copyright © 2025 schoolcompany. All rights reserved.
+//
+
+import Foundation
