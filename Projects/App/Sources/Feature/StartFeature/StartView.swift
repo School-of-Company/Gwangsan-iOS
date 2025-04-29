21450f35ee30d1228f59e13e335fcfc41ad627ee
hotcat0421 <s24044@gsm.hs.kr>
2025-04-29 12:00:49 +0900

    시작뷰 코드 수정

--- Projects/App/Sources/Feature/StartFeature/StartView.swift
+++ Projects/App/Sources/Feature/StartFeature/StartView.swift
@@ -12,13 +12,33 @@ struct StartView: View {
     var body: some View {
         NavigationView {
             VStack{
-                NavigationLink (destination: AliasInputView()) {
-                    Text("로그인")
-                }
+                
+                Spacer()
+                
                 NavigationLink (destination: NicknameInputView()) {
                     Text("회원가입")
+                        .frame(maxWidth: .infinity)
+                        .gwangsanFont(style: .body1)
+                        .frame(height: 52)
+                        .foregroundColor(GwangsanAsset.Color.mainGreen500.swiftUIColor)
+                        .overlay(
+                            RoundedRectangle(cornerRadius: 12)
+                                .stroke(GwangsanAsset.Color.mainGreen500.swiftUIColor, lineWidth: 1)
+                        )
+                        .padding(.horizontal, 24)
+                }
+                NavigationLink (destination: AliasInputView()) {
+                    Text("로그인")
+                        .frame(maxWidth: .infinity)
+                        .gwangsanFont(style: .body1)
+                        .frame(height: 52)
+                        .background(GwangsanAsset.Color.mainGreen500.swiftUIColor)
+                        .foregroundColor(.white)
+                        .cornerRadius(12)
+                        .padding(.horizontal, 24)
                 }
             }
+            .padding(.bottom, 30)
         }
         .navigationBarHidden(true)
     }
