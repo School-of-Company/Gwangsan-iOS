49c269ea445cd38ab62f6e69ce95f891a4f973d6
hotcat0421 <s24044@gsm.hs.kr>
2025-04-29 19:59:33 +0900

    💄 :: [#19] Tabbar 구현

---
+++ Projects/App/Sources/Feature/ChatFeature/ChatView.swift
@@ -0,0 +1,19 @@
+//
+//  ChatView.swift
+//  Gwangsan
+//
+//  Created by 박정우 on 4/29/25.
+//  Copyright © 2025 schoolcompany. All rights reserved.
+//
+
+import SwiftUI
+
+struct ChatView: View {
+    var body: some View {
+        Text("채팅 페이지")
+    }
+}
+
+#Preview {
+    ChatView()
+}
---
+++ Projects/App/Sources/Feature/ProfileFeature/ProfileView.swift
@@ -0,0 +1,19 @@
+//
+//  ProfileView.swift
+//  Gwangsan
+//
+//  Created by 박정우 on 4/29/25.
+//  Copyright © 2025 schoolcompany. All rights reserved.
+//
+
+import SwiftUI
+
+struct ProfileView: View {
+    var body: some View {
+        Text("프로필 페이지")
+    }
+}
+
+#Preview {
+    ProfileView()
+}
