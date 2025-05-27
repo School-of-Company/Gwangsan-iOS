4f8907ead96da22599b83267a0955ac37d5c0e49
hotcat0421 <s24044@gsm.hs.kr>
2025-05-27 09:55:44 +0900

    [#27] 아이템 모델에 content 추가 및 상세페이지 구현

--- Projects/App/Sources/Feature/DetailFeature/Views/ItemDetailView.swift
+++ Projects/App/Sources/Feature/DetailFeature/Views/ItemDetailView.swift
@@ -10,34 +10,134 @@ import SwiftUI
 
 struct ItemDetailView: View {
     let item: CommonItem
-
+    @Environment(\.dismiss) private var dismiss
+    
     var categoryText: String {
         item.category.displayName(for: item.mode)
     }
-
+    
     var body: some View {
-        VStack(spacing: 24) {
-            Image(item.imageName)
-                .resizable()
-                .aspectRatio(contentMode: .fit)
-                .frame(height: 200)
-                .cornerRadius(12)
-
-            Text(item.title)
-                .font(.title2)
-                .fontWeight(.bold)
-
-            Text("\(item.point) 광산")
-                .font(.body)
-                .foregroundColor(.gray)
-
-            Text("카테고리: \(categoryText)")
-                .font(.subheadline)
-                .foregroundColor(.secondary)
-
-            Spacer()
+        NavigationStack {
+            VStack {
+                ZStack {
+                    Text("물건")
+                        .gwangsanFont(style: .body1)
+                    
+                    HStack {
+                        Button(action: { dismiss() }) {
+                            Image(systemName: "chevron.left")
+                                .frame(width: 24, height: 24)
+                                .gwangsanColor(GwangsanAsset.Color.gray500)
+                        }
+                        Spacer()
+                    }
+                }
+                .padding(.horizontal, 24)
+                
+                Image(item.imageName)
+                    .resizable()
+                    .frame(height: 280)
+                    .frame(maxWidth: .infinity)
+                
+                HStack(alignment: .center) {
+                    HStack(spacing: 12) {
+                        Image(systemName: "person.crop.circle.fill")
+                            .resizable()
+                            .frame(width: 48, height: 48)
+                            .foregroundColor(.gray)
+                            .clipShape(Circle())
+                        
+                        VStack(alignment: .leading, spacing: 4) {
+                            Text("모태환")
+                                .gwangsanFont(style: .body3)
+                            
+                            Text("첨단 1동")
+                                .font(.system(size: 14))
+                                .foregroundColor(.gray)
+                        }
+                    }
+                    
+                    Spacer()
+                    
+                    Text("8단계")
+                        .gwangsanFont(style: .body1)
+                        .gwangsanColor(GwangsanAsset.Color.mainYellow500)
+                }
+                .padding(.horizontal, 24)
+                .padding(.vertical, 6)
+                
+                Divider()
+                
+                
+                VStack(alignment: .leading, spacing: 24) {
+                    
+                    Text(item.title)
+                        .gwangsanFont(style: .titleSmall)
+                    
+                    Text("\(item.point) 광산")
+                        .gwangsanFont(style: .body3)
+                    
+                    Text(item.content)
+                    
+                    Button(action: {
+                        print("신고 버튼 눌림")
+                    }) {
+                        Text("이 게시글 신고하기")
+                            .font(.footnote)
+                            .foregroundColor(.red)
+                            .overlay(
+                                Rectangle()
+                                    .frame(height: 1)
+                                    .foregroundColor(.red)
+                                    .offset(y: 2),
+                                alignment: .bottom
+                            )
+                    }
+                    .padding(.top, 25)
+                    
+                    Spacer()
+                }
+                .padding(.horizontal, 24)
+            }
+            VStack{
+                HStack(spacing: 12){
+                    GwangsanButton(
+                        text: "채팅하기",
+                        fontSize: 14,
+                        buttonState: true,
+                        horizontalPadding: 0,
+                        height: 52,
+                        style: .outline,
+                        destination: SwiftUIView() // 채팅뷰로 이동
+                    )
+                    
+                    GwangsanButton(
+                        text: "거래완료",
+                        fontSize: 14,
+                        buttonState: true,
+                        horizontalPadding: 0,
+                        height: 52,
+                        style: .filled,
+                        destination: SwiftUIView()
+                    )
+                }
+            }
+            .padding(.horizontal, 24)
         }
-        .padding()
-        .navigationTitle(categoryText)
+        .navigationBarHidden(true)
     }
+}
+
+#Preview {
+    ItemDetailView(
+        item: CommonItem(
+            id: UUID(),
+            title: "집 청소좀 해주세요",
+            point: 5000,
+            category: .request,
+            imageName: "TestImage1",
+            content: "집 청소좀 해주세요집 청소좀 해주세요집 청소좀 해주세요집 청소좀 해주세요집 청소좀 해주세요집 청소좀 해주세요",
+            mode: .service
+        )
+    )
 }
