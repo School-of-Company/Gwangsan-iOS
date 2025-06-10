27550faa562f6756f31aaf761340c525b6a8962d
hotcat0421 <s24044@gsm.hs.kr>
2025-06-10 20:50:47 +0900

    [#33] SignUpViewModel인수 전달

--- Projects/App/Sources/Feature/SignUpFeature/ViewModel/PhoneVerificationViewModel.swift
+++ Projects/App/Sources/Feature/SignUpFeature/ViewModel/PhoneVerificationViewModel.swift
@@ -1,19 +1,19 @@
 //
-//  SignUpFlowView.swift
+//  PhoneVerificationViewModel.swift
 //  Gwangsan
 //
-//  Created by 박정우 on 6/7/25.
+//  Created by 박정우 on 6/9/25.
 //  Copyright © 2025 schoolcompany. All rights reserved.
 //
 
 import SwiftUI
 
-struct SignUpFlowView: View {
+struct PhoneVerificationViewModel: View {
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
 }
 
 #Preview {
-    SignUpFlowView()
+    PhoneVerificationViewModel()
 }
