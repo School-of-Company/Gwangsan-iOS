7a71332be4c964dfc6029da1d01b362aef0c4691
hotcat0421 <s24044@gsm.hs.kr>
2025-06-15 16:01:30 +0900

    [#37] 내 정보 수정 페이지 퍼블리싱

--- Projects/App/Sources/Feature/ProfileFeature/Views/ProfileView/Views/ModifyProfileView.swift
+++ Projects/App/Sources/Feature/ProfileFeature/Views/ProfileView/Views/ModifyProfileView.swift
@@ -9,8 +9,73 @@
 import SwiftUI
 
 struct ModifyProfileView: View {
+    @Environment(\.dismiss) private var dismiss
+    @State var nickname: String = "" // 뷰모델
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack{
+            VStack{
+                ZStack {
+                    HStack {
+                        Button(action: { dismiss() }) {
+                            Image("Back")
+                        }
+                        Spacer()
+                        Text("내 정보 수정")
+                            .gwangsanFont(style: .body1)
+                        Spacer()
+                    }
+                    
+                    HStack {
+                        Spacer()
+                        Image("Close")
+                            .resizable()
+                            .frame(width: 25, height: 25)
+                    }
+                }
+                VStack(spacing: 25) {
+                    GwangsanTextField(
+                        "별칭을 입력해주세요",
+                        text: $nickname,
+                        title: "별칭",
+                        horizontalPadding: 0
+                    )
+                    .padding(.top, 25)
+                    GwangsanTextField(
+                        "별칭을 입력해주세요",
+                        text: $nickname,
+                        title: "별칭",
+                        horizontalPadding: 0
+                    )
+                    GwangsanTextField(
+                        "별칭을 입력해주세요",
+                        text: $nickname,
+                        title: "별칭",
+                        horizontalPadding: 0
+                    )
+                    GwangsanTextField(
+                        "별칭을 입력해주세요",
+                        text: $nickname,
+                        title: "별칭",
+                        horizontalPadding: 0
+                    )
+                }
+            }
+            .padding(.horizontal, 24)
+            
+            Spacer()
+            
+            GwangsanButton(
+                text: "수정",
+                buttonState: true,  // 필드값이 다 입력되면 true
+                horizontalPadding: 20,
+                height: 56,
+                action: {
+                    // 뷰모델 검사후 서버로 ㄱㄱ
+                }
+            )
+            .padding(.bottom, 30)
+            
+        }
     }
 }
 
