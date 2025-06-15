ceb8ec03f30a80dbea4a55a9409f7097bbcaaef4
hotcat0421 <s24044@gsm.hs.kr>
2025-06-15 16:00:47 +0900

    [#37] 내 정보 수정 페이지 퍼블리싱

---
+++ Projects/App/Sources/Feature/ProfileFeature/Views/ProfileView/Views/ModifyProfileView.swift
@@ -0,0 +1,19 @@
+//
+//  ModifyProfileView.swift
+//  Gwangsan
+//
+//  Created by 박정우 on 6/14/25.
+//  Copyright © 2025 schoolcompany. All rights reserved.
+//
+
+import SwiftUI
+
+struct ModifyProfileView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ModifyProfileView()
+}
