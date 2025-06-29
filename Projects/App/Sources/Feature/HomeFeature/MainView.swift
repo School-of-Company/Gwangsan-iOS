deab7a7e17dccb346c0137d95a6ff56cc2f02943
hotcat0421 <s24044@gsm.hs.kr>
2025-06-29 16:43:25 +0900

    [#49] MainPage 퍼블리싱 완료

--- Projects/App/Sources/Feature/HomeFeature/MainView.swift
+++ Projects/App/Sources/Feature/HomeFeature/MainView.swift
@@ -9,57 +9,72 @@
 import SwiftUI
 
 struct MainView: View {
+    let mainImages = ["Main1", "Main2", "Main3", "Main4", "Main5", "Main6"]
+    @State private var currentIndex = 0
+    private let timer = Timer.publish(every: 4, on: .main, in: .common).autoconnect()
+
     var body: some View {
         NavigationStack {
             ZStack {
                 Color.clear
-                VStack(alignment: .leading){
-                    HStack{
-                        VStack{
-                            HStack(spacing:0){
-                                Text("시민")
-                                    .gwangsanColor(GwangsanAsset.Color.mainBlue500)
-                                Text("화폐,")
-                                    .gwangsanColor(GwangsanAsset.Color.mainYellow500)
-                                Text("광산")
-                                    .gwangsanColor(GwangsanAsset.Color.mainGreen500)
-                            }
+                VStack(alignment: .leading) {
+                    HStack {
+                        HStack(spacing: 0) {
+                            Text("시민")
+                                .gwangsanColor(GwangsanAsset.Color.mainBlue500)
+                            Text("화폐,")
+                                .gwangsanColor(GwangsanAsset.Color.mainYellow500)
+                            Text("광산")
+                                .gwangsanColor(GwangsanAsset.Color.mainGreen500)
                         }
                         .font(.custom("Cafe24SsurroundOTF", size: 16))
-                        
+                        .padding(.vertical, 10)
+
                         Spacer()
-                        
+
                         Image("Bell")
                             .frame(width: 24, height: 24)
                     }
                     .padding(.horizontal, 24)
-                    
+
                     Rectangle()
-                        .frame(width: .infinity, height: 1)
+                        .frame(height: 1)
                         .gwangsanColor(GwangsanAsset.Color.gray400)
                     
-                    Image("TestImage1")
-                        .resizable()
-                        .aspectRatio(contentMode: .fill)
-                        .frame(width: .infinity, height: 250)
-                        .clipped()
-                        .padding(.vertical, 24)
-                    
+                    TabView(selection: $currentIndex) {
+                        ForEach(0..<mainImages.count, id: \.self) { index in
+                            Image(mainImages[index])
+                                .resizable()
+                                .scaledToFit()
+                                .frame(height: 250)
+                                .clipped()
+                                .tag(index)
+                                .transition(.slide)
+                        }
+                    }
+                    .tabViewStyle(PageTabViewStyle(indexDisplayMode: .never))
+                    .frame(height: 250)
+                    .onReceive(timer) { _ in
+                        withAnimation {
+                            currentIndex = (currentIndex + 1) % mainImages.count
+                        }
+                    }
+
                     Rectangle()
-                        .frame(width: .infinity, height: 1)
+                        .frame(height: 1)
                         .gwangsanColor(GwangsanAsset.Color.gray400)
                         .padding(.bottom, 30)
-                    
+
                     VStack(alignment: .leading) {
-                        Text("광산구도시재생센터") // 본점
+                        Text("광산구도시재생센터")
                             .font(.system(size: 24))
                             .bold()
-                        Text("수완세영") // 지점
+                        Text("수완세영")
                             .font(.system(size: 20))
                     }
-                    .padding(.horizontal,24)
+                    .padding(.horizontal, 24)
                     .padding(.bottom, 60)
-                    
+
                     HStack {
                         NavigationLink(destination: ServiceListView()) {
                             ZStack {
@@ -71,27 +86,24 @@ struct MainView: View {
                                     Image("Object")
                                     Text("물건")
                                         .font(.custom("Cafe24SsurroundOTF", size: 24))
-                                        .multilineTextAlignment(.center)
                                         .foregroundColor(.black)
                                 }
                             }
                             .shadow(color: .black.opacity(0.25), radius: 10, x: 2, y: 2)
                         }
-                        
+
                         Spacer()
-                        
+
                         NavigationLink(destination: ItemListView()) {
                             ZStack {
                                 Rectangle()
                                     .fill(.white)
                                     .frame(width: 160, height: 160)
                                     .cornerRadius(12)
-                                
                                 VStack(spacing: 20) {
                                     Image("Service")
                                     Text("서비스")
                                         .font(.custom("Cafe24SsurroundOTF", size: 24))
-                                        .multilineTextAlignment(.center)
                                         .foregroundColor(.black)
                                 }
                             }
@@ -99,7 +111,7 @@ struct MainView: View {
                         }
                     }
                     .padding(.horizontal, 24)
-                    
+
                     Spacer()
                 }
             }
@@ -107,6 +119,7 @@ struct MainView: View {
         .navigationBarHidden(true)
     }
 }
+
 
 #Preview {
     MainView()
